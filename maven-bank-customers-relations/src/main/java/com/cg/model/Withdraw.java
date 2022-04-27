f00d7773db2c50e26ddf9ddf4fda6d9201aa5063
Truong <vjc.ichi.149@gmail.com>
2022-04-27 16:05:41 +0700

    Banking-relations-maven ver 1.0

---
+++ maven-bank-customers-relations/src/main/java/com/cg/model/Withdraw.java
@@ -0,0 +1,34 @@
+package com.cg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "withdraws")
+@Accessors(chain = true)
+public class Withdraw extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+
+    @Digits(integer = 12, fraction = 0)
+    @Column(name = "transaction_amount", nullable= false)
+    private BigDecimal transactionAmount;
+}
